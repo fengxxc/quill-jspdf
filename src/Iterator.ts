ffd9883d1e79c721c7c189de077b7ce9e477d52c
fengxxc <fengxxc@gmail.com>
2025-04-03 20:22:04 +0800

    feat: add consume method

--- src/Iterator.ts
+++ src/Iterator.ts
@@ -6,7 +6,7 @@ interface StartAlignAttr {
 }
 
 export default class Iterator {
-    static BR_LEN = `\\n`.length;
+    static BR_LEN = `\n`.length;
 
     private _queueCache = new Array<Op>();
     private _lastHasBrOpIdx = -1;
@@ -37,6 +37,53 @@ export default class Iterator {
             this._lastBrIdx = lastBrIdx;
         }
         this._queueCache.push(op);
+    }
+
+    private hasNext(): boolean {
+        return this._queueCache.length > 0;
+    }
+
+    private take(): Op {
+        return this._queueCache.shift()!;
+    }
+
+    public static isAlignStart(op: Op) {
+        return op.attributes && "_start_align" in op.attributes;
+    }
+
+    public static getBrLatter(str: string, brIndex: number) {
+        return str.slice(brIndex + Iterator.BR_LEN);
+    }
+
+    public consume(): Op[] {
+        if (!this.hasNext()) {
+            return [];
+        }
+        const op = this.take();
+        if (Iterator.isAlignStart(op)) {
+            return Iterator.splitOpByLastBr(op);
+        }
+        return [op];
+    }
+
+    /**
+     * 裂解出一个带开始标识的、换行符后的内容
+     * @param op 
+     * @returns 
+     */
+    public static splitOpByLastBr(op: Op): Op[] {
+        const saa = op.attributes!["_start_align"] as StartAlignAttr;
+        if (typeof op.insert !== "string") {
+            return [op]; // TODO
+        }
+        const newText = Iterator.getBrLatter(op.insert as string, saa.index);
+        op.insert = op.insert.slice(0, saa.index + Iterator.BR_LEN);
+        delete op.attributes!["_start_align"]
+        const newOp = {
+            insert: newText,
+            attributes: { ...op.attributes, start_align: saa.align }
+        } as Op;
+        return [op, newOp];
     }
 
     public getQueueCache(): Array<Op> {
